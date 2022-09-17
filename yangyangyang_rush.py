6d07a121a1abf08eff15cf8b615505e5d084505f
silva <739146106@qq.com>
2022-09-17 23:50:41 +0800

    Update yangyangyang_rush.py
    修复bug

--- yangyangyang_rush.py
+++ yangyangyang_rush.py
@@ -15,10 +15,11 @@ requests.packages.urllib3.disable_warnings()
 
 headers = {
     'Host': 'cat-match.easygame2021.com',
-    # 'Connection': 'keep-alive',
+    'Connection': 'keep-alive',
     'content-type': 'application/json',
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.26(0x18001a34) NetType/WIFI Language/zh_CN',
     'Referer': 'https://servicewechat.com/wx141bfb9b73c970a9/15/page-frame.html',
+    't': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQ1MDI0NDUsIm5iZiI6MTY2MzQwMDI0NSwiaWF0IjoxNjYzMzk4NDQ1LCJqdGkiOiJDTTpjYXRfbWF0Y2g6bHQxMjM0NTYiLCJvcGVuX2lkIjoiIiwidWlkIjo0NTk0MjYwMiwiZGVidWciOiIiLCJsYW5nIjoiIn0.1lXIcb1WL_SdsXG5N_i1drjjACRhRZUS2uadHlT6zIY'
 }
 
 # 异常处理 无线重试
@@ -50,10 +51,9 @@ def except_output(msg='异常', retry_num=10, is_while=True):
 
 @except_output(msg='fetch_wx_union_id', retry_num=5)
 def fetch_wx_union_id(user_id):
-    t = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTQ0NzgwMTAsIm5iZiI6MTY2MzM3NTgxMCwiaWF0IjoxNjYzMzc0MDEwLCJqdGkiOiJDTTpjYXRfbWF0Y2g6bHQxMjM0NTYiLCJvcGVuX2lkIjoiIiwidWlkIjo0ODUwNDY1MCwiZGVidWciOiIiLCJsYW5nIjoiIn0.IEjNoHJiJPqlh86DqDS3-SMTwErTCatQF6ykZk4o-Yc"
-    url = f"https://cat-match.easygame2021.com/sheep/v1/game/user_info?uid={user_id}&t={t}"
-    response = requests.get(url, headers=headers, verify=False)
-    wx_union_id = response.json()['data']['wx_union_id']
+    response = requests.get(f"https://cat-match.easygame2021.com/sheep/v1/game/user_info?uid={user_id}", headers=headers)
+    print(response.json()['data'])
+    wx_union_id = response.json()['data']['wx_open_id']
     return wx_union_id
 
 @except_output(msg='fetch_token', retry_num=5)
@@ -62,8 +62,6 @@ def fetch_token(wx_union_id):
     data = {"uuid": wx_union_id}
     res = requests.post(tokenUrl, data, verify=False).json()
     return res['data']['token']
-
-
 
 
 class YangYang:
@@ -122,4 +120,4 @@ class YangYang:
             print('通关完成！')
 
 if __name__ == '__main__':
-    YangYang('0210201', count=9999).go_pass()
+    YangYang('xxxx', count=100).go_pass()
